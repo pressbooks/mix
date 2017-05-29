a704da0795a6714192cba2a7e123ef9b56837005
Ned Zimmerman <ned@bight.ca>
2017-05-29 11:49:26 -0300

    Remove hot functionality, add docs.

---
+++ README.md
@@ -0,0 +1,3 @@
+# pressbooks/mix
+
+The `PressbooksMix\Assets` class provides an asset path function for WordPress plugins that use [Laravel Mix](https://github.com/JeffreyWay/laravel-mix/).
--- inc/Assets.php
+++ inc/Assets.php
@@ -2,13 +2,38 @@
 namespace PressbooksMix;
 
 class Assets {
+	/**
+   * The plugin slug.
+   *
+   * @type string
+   */
 	public static $pluginSlug;
 
-	public function __construct( $slug ) {
-		self::$pluginSlug = $slug;
+	/**
+   * The directory within the plugin where built assets are located.
+   *
+   * @type string
+   */
+	public static $distDirectory;
+
+	/**
+   * Constructor.
+   *
+   * @param string $plugin_slug
+   * @param string $dist_directory
+   */
+	public function __construct( $plugin_slug, $dist_directory = 'dist' ) {
+		self::$pluginSlug = $plugin_slug;
+		self::$distDirectory = $dist_directory;
 	}
 
-	public function getPath( $path, $dist_dirname = 'dist' ) {
+	/**
+   * Loads the mix-manifest.json file and returns the versioned URL of an asset located at $path.
+   *
+   * @param string $path
+   * @return string
+   */
+	public function getPath( $path ) {
 		static $manifest;
 
 		if ( substr( $path, 0, 1 ) !== '/' ) {
@@ -17,25 +42,20 @@ class Assets {
 
 		$root_dir = trailingslashit( WP_PLUGIN_DIR );
 
-		if ( file_exists( $root_dir . self::$pluginSlug . '/hot' ) ) {
-			return 'http://localhost:8080' . $path;
-		}
-
 		if ( ! $manifest ) {
-			$manifest_path = $root_dir . trailingslashit( self::$pluginSlug ) . $dist_dirname . '/mix-manifest.json';
+			$manifest_path = $root_dir . trailingslashit( self::$pluginSlug ) . self::$distDirectory . '/mix-manifest.json';
 
 			if ( ! file_exists( $manifest_path ) ) {
-				return plugins_url( trailingslashit( self::$pluginSlug ) . $dist_dirname . $path );
+				return plugins_url( trailingslashit( self::$pluginSlug ) . self::$distDirectory . $path );
 			}
 
 			$manifest = json_decode( file_get_contents( $manifest_path ), true );
 		}
 
 		if ( ! array_key_exists( $path, $manifest ) ) {
-			return plugins_url( trailingslashit( self::$pluginSlug ) . $dist_dirname . $path );
+			return plugins_url( trailingslashit( self::$pluginSlug ) . self::$distDirectory . $path );
 		}
 
-		return plugins_url( trailingslashit( self::$pluginSlug ) . $dist_dirname . $manifest[ $path ] );
-
+		return plugins_url( trailingslashit( self::$pluginSlug ) . self::$distDirectory . $manifest[ $path ] );
 	}
 }
